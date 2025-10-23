c37480879eb62bf4c7d9f4b6874a99a0cbcdf98e
Art3mis17 <saranmass234@gmail.com>
2025-10-23 10:44:22 +0530

    first commit

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ app.get('/', (req, res) => {
   res.send('Hello from Blue-Green Deployment!');
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
